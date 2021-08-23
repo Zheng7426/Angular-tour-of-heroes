64952f763c54f0c11c64c76df7c45ce9f4b866e4
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-08-23 21:51:33 +0800

    hero list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Who'},
+    { id: 12, name: 'Mr Robot'},
+    { id: 13, name: 'Luffy'},
+    { id: 14, name: 'Usopp'},
+    { id: 15, name: 'Spiderman'},
+    { id: 16, name: 'Storm'},
+    { id: 17, name: 'Wolverine'},
+    { id: 18, name: 'Ironman'},
+    { id: 19, name: 'Captain Marvel'}
+];

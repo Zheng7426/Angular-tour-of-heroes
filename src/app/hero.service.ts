16d205dde6b91a98b26da5b570a3aac575e3ade2
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-08-25 17:23:24 +0800

    error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,8 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 // Fetching data from remote server
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+// Error handling
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -34,13 +36,31 @@ export class HeroService {
   //   this.log('fetched heroes');
   //   return heroes;
   // }
+  // getHeroes(): Observable<Hero[]> {
+  //   return this.http.get<Hero[]>(this.heroesUrl);
+  // }
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
     this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  // Handle Http operation that failed.
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }

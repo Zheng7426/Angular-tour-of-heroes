0cac5d03917a07417b1bd4ba4ebfd0ff41423081
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-08-25 18:04:17 +0800

    add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -79,6 +79,14 @@ export class HeroService {
       );
   }
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`added hero with id=${newHero.id}`)
+      ), catchError(this.handleError<Hero>('addHero')));
+  }
+
   // Handle Http operation that failed.
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
